f6f3bec7e168e0bebd82420ca8f0619e60aa68d6
henry-kanengiser <49458585+henry-kanengiser@users.noreply.github.com>
2024-06-04 23:26:39 -0400

    update to do list

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
 // TO DO LIST
-// - Figure out way to set up buttons to select each subway route (will need to link the button to a particular feature id)
 // - Check underlying census tract data (seems to be missing for parts of SI and the 4 train)
-// - Maybe move button list into the overlay accordion to keep the screen cleaner
 // - Add in datawrapper interactive figure of income & population by train line
 // - Reevaluate the 2017 data, is it really helpful? May want to just focus on 2022
 // - Hover popup for census tracts above a certain zoom that shows geoid, population, & income?
